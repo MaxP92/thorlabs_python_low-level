7a805ae30bc235bad23febde1adeb3affe084a84
MaxP92 <pinsard.maxime@gmail.com>
2018-01-23 09:48:03 -0500

    Add files via upload

--- example_move_sequence.py
+++ example_move_sequence.py
@@ -9,6 +9,8 @@ Created on Aug 18 15:35:13 2017
 
 # copy/paste (or execute) here all the init functions
 
+import time
+
 ## move sequence
 
 abs_dist = 50.5 # mm
@@ -18,6 +20,8 @@ nb_move = 60
 
 verbose = 1
 blocking = True # blocks after each move
+tot_bytes2read = 20 
+time_out_motion = 2 # sec
 
 for i in range (nb_move):
     if i%2: # odd
@@ -26,10 +30,20 @@ for i in range (nb_move):
         command_moveAbs1 = thorlabs_lowlvl_list.commandGen_moveAbsXY_meth(1, abs_dist2)
 
     motor_stageXY.write(command_moveAbs1)
+        
     if blocking: # wait for MGMSG_MOT_MOVE_COMPLETED
-        bb = motor_stageXY.read(20)
-        if verbose:
-            pos , stat = thorlabs_lowlvl_list.msg_decoder_movecompleted_XY_meth(bb) 
-            print('Move pos is %s with state %s', pos , stat)
+        bb = b'' # binary empty
+        st_time = time.time()
+        while (len(bb) < tot_bytes2read): # not bool(bb):
+        
+            if (time_out_motion > 0 and (time.time() - st_time) >= time_out_motion): 
+                print('\n read move complete timed out for motor %d !! \n' % str_motor_stop)
+                print(bb)
+                break
             
-        motor_stageXY.write(thorlabs_lowlvl_list.command_serv_alive1) # say that server is alive
+            bb_new = motor_stageXY.read(tot_bytes2read - len(bb)) # try to read the MOVE_COMPLETED string, during time_out seconds only (short time) !
+            motor_stageXY.write(thorlabs_lowlvl_list.command_serv_alive1)
+            bb = bb + bb_new
+            # # print(bb)
+                
+            
